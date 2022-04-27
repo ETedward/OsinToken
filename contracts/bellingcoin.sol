936d1f1ce8b4ad8bc9f7192ffbae383e2ed45a2e
liamdj <liamdj@princeton.edu>
2022-04-27 16:29:23 -0400

    Naming style

--- contracts/bellingcoin.sol
+++ contracts/bellingcoin.sol
@@ -10,32 +10,31 @@ contract Token {
     string public constant name = "Bellingcoin";
     uint public constant seconds_for_nominations = 172800;
     uint public constant seconds_for_voting = 432000;
-    uint public start_time;
-    bool public voting_open;
+    uint public startTime;
 
-    mapping(address => uint16) public goverance_balance;
-    uint16 public goverance_supply;
+    mapping(address => uint16) public goveranceBalance;
+    uint16 public goveranceSupply;
 
-    mapping(address => uint16) public reward_balance;
-    uint16 public reward_supply;
+    mapping(address => uint16) public rewardBalance;
+    uint16 public rewardSupply;
 
     Article[] public winners;
 
     Article[] public nominees;
     // number of nominations made by each address
     address[] nominators;
-    mapping(address => uint16) nominations_made;
+    mapping(address => uint16) nominationsMade;
     // number of votes cast by each address for each nominee
     address[] voters;
-    mapping(address => uint16[]) votes_cast;
+    mapping(address => uint16[]) votesCast;
     // cumulative votes for each nominee
-    uint16[] public vote_counts;
+    uint16[] public voteCounts;
 
     constructor(uint16 _supply, uint _start) public {
-        goverance_supply = _supply;
-        goverance_balance[msg.sender] = _supply;
-        start_time = _start;
-        reward_supply = 0;
+        goveranceSupply = _supply;
+        goveranceBalance[msg.sender] = _supply;
+        startTime = _start;
+        rewardSupply = 0;
     }
 
     event Nominate(address indexed _nominator, address indexed _writer, string _url);
@@ -53,24 +52,24 @@ contract Token {
     }
 
     function votingEnds() public view returns (uint) {
-        return start_time + seconds_for_nominations + seconds_for_voting;
+        return startTime + seconds_for_nominations + seconds_for_voting;
     }
 
     function votesRemaining(address _voter) public view returns (uint16) {
         uint16 cast = 0;
         for (uint16 i = 0; i < nominees.length; i++) {
-            cast += votes_cast[_voter][i];
+            cast += votesCast[_voter][i];
         }
-        return goverance_balance[_voter] + reward_balance[_voter] - cast;
+        return goveranceBalance[_voter] + rewardBalance[_voter] - cast;
     }
 
     function submitNomination(address _writer, string calldata _url) public returns (bool) {
-        require(nominations_made[msg.sender] < goverance_balance[msg.sender]);
-        require(block.timestamp > start_time);
-        require(block.timestamp <= start_time + seconds_for_nominations);
+        require(nominationsMade[msg.sender] < goveranceBalance[msg.sender]);
+        require(block.timestamp > startTime);
+        require(block.timestamp <= startTime + seconds_for_nominations);
         require(!alreadyNominated(_url));
 
-        nominations_made[msg.sender] += 1;
+        nominationsMade[msg.sender] += 1;
         nominees.push(Article(_writer, _url));
         nominators.push(msg.sender);
 
@@ -80,11 +79,11 @@ contract Token {
 
     function castVotes(uint16 _index, uint16 _value) public returns (bool) {
         require(votesRemaining(msg.sender) >= _value);
-        require(block.timestamp > start_time + seconds_for_nominations);
-        require(block.timestamp <= start_time + seconds_for_nominations + seconds_for_voting);
+        require(block.timestamp > startTime + seconds_for_nominations);
+        require(block.timestamp <= startTime + seconds_for_nominations + seconds_for_voting);
 
-        votes_cast[msg.sender][_index] += _value;
-        vote_counts[_index] += _value;
+        votesCast[msg.sender][_index] += _value;
+        voteCounts[_index] += _value;
         voters.push(msg.sender);
 
         emit Vote(msg.sender, nominees[_index].url, _value);
@@ -94,8 +93,8 @@ contract Token {
     function transferGoverance(address _to, uint16 _value) public returns (bool) {
         require(votesRemaining(msg.sender) >= _value);
 
-        goverance_balance[msg.sender] -= _value;
-        goverance_balance[_to] += _value;
+        goveranceBalance[msg.sender] -= _value;
+        goveranceBalance[_to] += _value;
 
         emit Transfer_Goverance(msg.sender, _to, _value);
         return true;
@@ -104,26 +103,27 @@ contract Token {
     function transferReward(address _to, uint16 _value) public returns (bool) {
         require(votesRemaining(msg.sender) >= _value);
 
-        reward_balance[msg.sender] -= _value;
-        reward_balance[_to] += _value;
+        rewardBalance[msg.sender] -= _value;
+        rewardBalance[_to] += _value;
 
         emit Transfer_Reward(msg.sender, _to, _value);
         return true;
     }
 
     function updateWinner() public returns (bool) {
-        if (block.timestamp > start_time + seconds_for_nominations + seconds_for_voting) {
+        if (block.timestamp > startTime + seconds_for_nominations + seconds_for_voting) {
             if (nominees.length > 0) {
                 uint16 index = 0;
                 for (uint16 i = 1; i < nominees.length; i++) {
-                    if (vote_counts[i] > vote_counts[index]) {
+                    if (voteCounts[i] > voteCounts[index]) {
                         index = i;
                     }
                 }
                 Article memory winner = nominees[index];
 
                 winners.push(winner);
-                reward_balance[winner.writer] += 1;
+                rewardBalance[winner.writer] += 1;
+                rewardSupply += 1;
                 emit Winner(winner.writer, winner.url);
             }
            
@@ -137,18 +137,18 @@ contract Token {
         delete nominees;
 
         for (uint16 i = 0; i < nominators.length; i++) {
-            delete nominations_made[nominators[i]];
+            delete nominationsMade[nominators[i]];
         }
         delete nominators;
 
         for (uint16 i = 0; i < voters.length; i++) {
-            delete votes_cast[voters[i]];
+            delete votesCast[voters[i]];
         }
         delete voters;
 
-        delete vote_counts;
+        delete voteCounts;
 
-        start_time += seconds_for_nominations + seconds_for_voting;
+        startTime += seconds_for_nominations + seconds_for_voting;
     }
 
     function alreadyNominated(string calldata _url) private view returns (bool) {
